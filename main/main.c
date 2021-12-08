44e286b58c782677d62602eba6fa5123d818fedc
bosun <tommy15433@gmail.com>
2021-12-08 15:42:37 +0900

    - reset button port uses internal pull-down

--- main/main.c
+++ main/main.c
@@ -161,7 +161,7 @@ void reset_gpio_init(void)
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pin_bit_mask = GPIO_RESET_PINSEL;
-    io_conf.pull_down_en = 0;
+    io_conf.pull_down_en = 1;
     io_conf.pull_up_en = 0;
 
     gpio_config(&io_conf);
